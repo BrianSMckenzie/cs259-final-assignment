d8e9d60125383f613c698b03e3587e98f7689cc5
Brian McKenzie <49575921+BrianSMckenzie@users.noreply.github.com>
2024-11-28 15:00:43 +0000

    Update project-template(2).java

--- project-template(2).java
+++ project-template(2).java
@@ -105,15 +105,15 @@ public class Tests {
 
     // ...
 
-    static int NumberOfFeatures = 10;
+    static int NumberOfFeatures = 11;
     static double[] toFeatureVector(double id, String genre, double runtime, double year, double imdb, double rt, double budget, double boxOffice) {
 
 
         double[] feature = new double[NumberOfFeatures];
-        
+
         switch (genre) { // We also use represent each movie genre as an integer number:
 
-            // had to change this as the original template had the array index and the 1s swapped x
+            // had to change this as the original template had the array index and the 1s swapped
             case "Action":  feature[0] = 1; break;
             case "Fantasy":   feature[1] = 1; break;
             case "Romance": feature[2] = 1; break;
@@ -125,9 +125,13 @@ public class Tests {
             default: Assert(false);
 
         }
-        // this is new went from 61 -> 66%
+
+        // this is new went from 61 -> 66% (if movie has good reviews better change Alex will like it)
         feature [8] = ( rt >= 70 ? 1 : 0) ; // if rotten tomatoes >= 70% then == 1
         feature [9] = ( imdb >= 7 ? 1 : 0) ; // if imdb >= 7 then == 1
+
+        // tried including box office didnt seem to make a difference
+
         // That is all. We don't use any other attributes for prediction.
         return feature;
     }
@@ -164,19 +168,19 @@ public class Tests {
                 String[] values = line.split(",");
                 // Assuming csv format: MovieID,Title,Genre,Runtime,Year,Lead Actor,Director,IMDB,RT(%),Budget,Box Office Revenue (in million $),Like it
                 // look at original template the formatting and parsing was all kinds of messed up
+                // changed most of the doubles to ints because they didnt need to be doubles i dont think
                 double id = Double.parseDouble(values[0]);
                 String title = values[1];
                 String genre = values[2];
                 int year = Integer.parseInt(values[3]);
                 String director = values[4];
                 String leadActor = values[5];
-                double rt = Double.parseDouble(values[6]);
+                int rt = Integer.parseInt(values[6]);
                 double imdb = Double.parseDouble(values[7]);
-                double boxOffice = Double.parseDouble(values[8]);
-                double budget = Double.parseDouble(values[9]);
-                double runtime = Double.parseDouble(values[10]);
+                int boxOffice = Integer.parseInt(values[8]);
+                int budget = Integer.parseInt(values[9]);
+                int runtime = Integer.parseInt(values[10]);
                 boolean likeit = Boolean.parseBoolean(values[11]);
-
 
                 dataFeatures[idx] = toFeatureVector(id, genre, runtime, year, imdb, rt, budget, boxOffice);
                 dataLabels[idx] = Integer.parseInt(values[11]); // Assuming the label is the last column and is numeric
